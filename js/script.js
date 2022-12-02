d05172f98e6d8fd1f2566c5ac94162f0ffe14454
eric <ecastellani@outlook.fr>
2022-12-02 16:52:34 +0100

    Exercices JS introduction

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+console.log("Bonjour")
+
+const boxes = document.querySelectorAll(".box")
+console.log(boxes)
+
+boxes.forEach((box, index)=>{
+    box.addEventListener("click", ()=>{
+    //box.style.backgroundcolor = "green"
+    //box.classList.add("active")
+    box.classList.toggle("active")
+    
+})})
+   
+
+console.log("boite n°"  +(index+1) + "cliquée");
+
+
+
